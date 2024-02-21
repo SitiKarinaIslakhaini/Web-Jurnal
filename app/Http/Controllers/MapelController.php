b7d334d37bc7da135c14cc2f77d64ec27bb7d249
SitiKarinaIslakhaini <sitikarinaislakhaini@gmail.com>
2024-02-21 11:14:59 +0700

    jurnal

---
+++ app/Http/Controllers/MapelController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\mapel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class MapelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mapel = mapel::latest()->paginate(5);
+
+        return view('mapel.index', compact('mapel'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('mapel.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nama_mapel'   => 'required',
+
+        ]);
+        mapel::create([
+            'nama_mapel'     => $request->nama_mapel,
+        ]);
+
+        //redirect to index
+        return redirect()->route('mapel.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit( mapel $mapel)
+    {
+        return view('mapel.edit', compact('mapel'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, mapel $mapel)
+    {
+        $this->validate($request, [
+            'nama_mapel'     => 'required|min:5',
+        ]);
+            $mapel->update([
+                'nama_mapel'     => $request->nama_mapel,
+            ]);
+        //redirect to index
+        return redirect()->route('mapel.index')->with(['success' => 'Data Berhasil Diubah!']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy( mapel $mapel)
+    {
+        $mapel->delete();
+
+        //redirect to index
+        return redirect()->route('mapel.index')->with(['success' => 'Data Berhasil Dihapus!']);
+    }
+}
---
+++ app/Models/pengajar.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pengajar extends Model
+{
+    use HasFactory;
+    protected $table = 'pengajar';
+
+    protected $fillable=[
+        'guru_id',
+        'mapel_id',
+        'kelas',
+        'jam_pelajaran',
+
+    ];
+    public function guru()
+    {
+        return $this->belongsTo(guru::class, 'guru_id');
+
+    }
+    public function mapel()
+    {
+        return $this->belongsTo(mapel::class, 'mapel_id');
+
+    }
+}
